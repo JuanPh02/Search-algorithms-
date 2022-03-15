328081a36e5cfda8282a40a5c24ca650cfd4c7ee
Juan Pablo Arroyave <50724493+JuanPh2002@users.noreply.github.com>
2022-03-15 17:26:19 -0500

    Algoritmos de busqueda

---
+++ profundidad.py
@@ -0,0 +1,43 @@
+values = {
+    "Pereira": "Medellin",
+    "Manizales": "Medellin",
+    "Puerto Berrio": "Medellin",
+    "Ibague": "Pereira",
+    "Honda": "Manizales",
+    "Tunja": "Puerto Berrio",
+    "Melgar": "Ibague",
+    "Facatativa": "Honda",
+    "Bogota": "Tunja",
+    "Bogota": "Melgar",
+    "Bogota": "Facatativa"
+}
+
+path = []
+
+
+def search(start, valueToFind):
+    path.append(start)
+    # si encontramos el valor, lo devolvemos
+    if start == valueToFind:
+        return valueToFind
+    # recorremos todos los elementos en busca del valor de start
+    for k, v in values.items():
+        result = 0
+        # si el valor del elemento tiene como padre al valor de start
+        if v == start:
+            # llamamos a la función recursivamente enviando el nuevo padre
+            result = search(k, valueToFind)
+
+        # si hemos recibido algun resultado es que hemos encontrado el elemento
+        if result:
+            return result
+
+    path.pop()
+    # si llegamos aqui, es que hemos llegado al final de una profundidad
+    return 0
+
+result = search("Medellin", "Bogota")
+if result:
+    print(path)
+else:
+    print("No existe")
